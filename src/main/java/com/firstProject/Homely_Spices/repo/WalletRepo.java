c12e19da61082c33b25d3fc4552659f2f3c4bac6
Athila Rahim <athilaraheem@gmail.com>
2025-03-03 16:09:00 +0530

    Updated native query for wallet transaction

--- src/main/java/com/firstProject/Homely_Spices/repo/WalletRepo.java
+++ src/main/java/com/firstProject/Homely_Spices/repo/WalletRepo.java
@@ -15,17 +15,23 @@ public interface WalletRepo extends JpaRepository<Wallet, Integer> {
     @Query("SELECT w FROM Wallet w WHERE w.user.id = :userId ORDER BY w.transactionDate DESC")
     List<Wallet> findWalletTransactionsByUserId(@Param("userId") int userId);
 
-    @Query(value = "SELECT COALESCE(MAX(wallet_balance), 0.0) FROM wallet WHERE user_id = :userId ORDER BY transaction_date DESC, id DESC LIMIT 1", nativeQuery = true)
+    @Query(value = "SELECT COALESCE(wallet_balance, 0.0) FROM wallet " +
+            "WHERE user_id = :userId " +
+            "ORDER BY transaction_date DESC, id DESC " +
+            "LIMIT 1",
+            nativeQuery = true)
     Double findLatestWalletBalanceByUserId(@Param("userId") int userId);
+
 
     @Query("SELECT COALESCE(SUM(w.transactionAmount), 0) FROM Wallet w WHERE w.transactionType = 'CREDIT'")
     Double findTotalCredits();
 
-    @Query("SELECT DATE(w.transactionDate), COALESCE(SUM(w.transactionAmount), 0) " +
-            "FROM Wallet w WHERE w.transactionType = 'CREDIT' " +
-            "AND DATE(w.transactionDate) BETWEEN :startDate AND :endDate " +
-            "GROUP BY DATE(w.transactionDate) " +
-            "ORDER BY DATE(w.transactionDate) ASC")
+    @Query(value = "SELECT DATE(transaction_date), COALESCE(SUM(transaction_amount), 0) " +
+            "FROM wallet WHERE transaction_type = 'CREDIT' " +
+            "AND DATE(transaction_date) BETWEEN :startDate AND :endDate " +
+            "GROUP BY DATE(transaction_date) " +
+            "ORDER BY DATE(transaction_date) ASC",
+            nativeQuery = true)
     List<Object[]> findDailyCreditsWithinRange(@Param("startDate") LocalDate startDate,
                                                @Param("endDate") LocalDate endDate);
 
